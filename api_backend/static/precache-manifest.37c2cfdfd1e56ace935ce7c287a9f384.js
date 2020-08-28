b5c135c6293ac70991d935c5a57a396d92bf4ad8
Your Name <you@example.com>
2020-08-28 11:46:57 +0300

    API so far

---
+++ api_backend/static/precache-manifest.37c2cfdfd1e56ace935ce7c287a9f384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df517ce8d64ec8bb31ce33f968be5f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2f7c3263932615e9bc1",
+    "url": "/static/css/main.96d9eb94.chunk.css"
+  },
+  {
+    "revision": "d629e5b7ae4b1bc50478",
+    "url": "/static/js/2.13fe617c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13fe617c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f7c3263932615e9bc1",
+    "url": "/static/js/main.b8cefe9b.chunk.js"
+  },
+  {
+    "revision": "a7fef38bf52eb3c5812a",
+    "url": "/static/js/runtime-main.52e2f213.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import querystring from "querystring";
+import "./App.css";
+
+const config = {
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+};
+
+const BACKEND_URL = "http://127.0.0.1:8000/login/token/acces-token";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin() {
+    const body = {
+      username: username,
+      password: password,
+    };
+    // The post request
+    axios
+      .post(BACKEND_URL, querystring.stringify(body), config)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((e) => console.log("Login failed"));
+  }
+
+  function handleUsernameChange(e) {
+    setUsername(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <form>
+          <div>
+            <label className="label" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="input"
+              id="username"
+              name="username"
+              placeholder="Username"
+              onChange={handleUsernameChange}
+            />
+          </div>
+
+          <div>
+            <label className="label" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="input"
+              id="password"
+              name="password"
+              placeholder="************"
+              type="password"
+              onChange={handlePasswordChange}
+            />
+          </div>
+
+          <div className="button-container">
+            <button className="button" type="button" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
